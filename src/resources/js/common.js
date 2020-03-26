e918275b639bce97ffee1205c0421c74724b7d68
Matthew Holt <mholt@users.noreply.github.com>
2020-03-26 13:00:57 -0600

    Disable debug mode on algolia search dropdown

--- src/resources/js/common.js
+++ src/resources/js/common.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 		apiKey: '14275a785f6ebd31d162f9d2d8fc0125',
 		indexName: 'caddyserver',
 		inputSelector: '#search',
-		debug: true // Set debug to true if you want to inspect the dropdown
+		debug: false // Set debug to true if you want to inspect the dropdown
 	});
 });
