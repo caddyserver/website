e0541c0f3f1fb39371b16861531dda48d0d1b71a
Francis Lavoie <lavofr@gmail.com>
2022-05-11 12:31:22 -0400

    docsearch: Upgrade to v3 (#231)
    See:
    - https://docsearch.algolia.com/docs/DocSearch-v3
    - https://docsearch.algolia.com/docs/migrating-from-v2

--- src/resources/js/common.js
+++ src/resources/js/common.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 	// Algolia search
 	docsearch({
+		appId: "BH4D9OD16A",
 		apiKey: '14275a785f6ebd31d162f9d2d8fc0125',
 		indexName: 'caddyserver',
-		inputSelector: '#search',
-		debug: false // Set debug to true if you want to inspect the dropdown
+		container: '#search',
 	});
 });
 
