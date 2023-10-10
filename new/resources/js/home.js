cb4141e9c13d33ba06cdd19baa03586a7309efe2
Matthew Holt <mholt@users.noreply.github.com>
2023-10-10 12:03:33 -0600

    Continue home and features pages

--- new/resources/js/home.js
+++ new/resources/js/home.js
@@ -1,5 +1,16 @@
-ready(async function() {
-	const resp = await fetch("/resources/testimonials.json");
-  	const testimonials = await resp.json();
+const resp = fetch("/resources/testimonials.json").then(async resp => {
+	const testimonials = await resp.json();
 	console.log(testimonials);
+
+	ready(function() {
+		for (let i = 0; i < testimonials.length; i++) {
+			const testimonial = testimonials[i];
+			const tpl = cloneTemplate('#tpl-testimonial');
+			$('.testimonial-picture', tpl).src = testimonial.picture || "";
+			$('.testimonial-quote', tpl).innerText = `"${testimonial.quote}"`;
+			$('.testimonial-name', tpl).innerText = testimonial.name || "";
+			$('.testimonial-role', tpl).innerText = testimonial.role || "";
+			$(`.testimonial-col:nth-child(${i%3 + 1})`).append(tpl);
+		}
+	});
 });
