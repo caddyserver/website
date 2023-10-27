616cc0eff11cab3292ac839aeb59fe9253728e07
Matthew Holt <mholt@users.noreply.github.com>
2023-10-26 22:24:46 -0600

    More homepage/features content, screenshots, tweaks

--- new/resources/js/home.js
+++ new/resources/js/home.js
@@ -8,8 +8,16 @@ const resp = fetch("/resources/testimonials.json").then(async resp => {
 			const tpl = cloneTemplate('#tpl-testimonial');
 			$('.testimonial-picture', tpl).src = testimonial.picture || "";
 			$('.testimonial-quote', tpl).innerText = `"${testimonial.quote}"`;
-			$('.testimonial-name', tpl).innerText = testimonial.name || "";
+			if (testimonial.username) {
+				$('.testimonial-name', tpl).innerText = ` @${testimonial.username}`;
+			} else {
+				$('.testimonial-name', tpl).innerText = testimonial.name || "";
+			}
 			$('.testimonial-role', tpl).innerText = testimonial.role || "";
+			if (testimonial.org) {
+				$('.testimonial-role', tpl).appendChild(document.createElement("br"));
+				$('.testimonial-role', tpl).appendChild(document.createTextNode(testimonial.org));
+			}
 			$(`.testimonial-col:nth-child(${i%3 + 1})`).append(tpl);
 		}
 	});
