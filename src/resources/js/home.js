3c22d632ed41b619b1758e50ff620b5edeb81ea3
Matthew Holt <mholt@users.noreply.github.com>
2024-02-19 16:39:35 -0700

    Improve testimonials

--- src/resources/js/home.js
+++ src/resources/js/home.js
@@ -7,18 +7,22 @@ const resp = fetch("/resources/testimonials.json").then(async resp => {
 			const tpl = cloneTemplate('#tpl-testimonial');
 			$_('.testimonial-picture', tpl).src = testimonial.picture || "";
 			$_('.testimonial-quote', tpl).innerText = `"${testimonial.quote}"`;
-			if (testimonial.username) {
-				$_('.testimonial-name', tpl).innerText = ` @${testimonial.username}`;
-			} else {
+			if (testimonial.name) {
 				$_('.testimonial-name', tpl).innerText = testimonial.name || "";
+			} else {
+				$_('.testimonial-name', tpl).innerText = ` @${testimonial.username}`;
 			}
 			if (testimonial.link) {
 				$_('.testimonial-name', tpl).href = testimonial.link;
 			}
-			$_('.testimonial-role', tpl).innerText = testimonial.role || "";
+			$_('.testimonial-role-text', tpl).innerText = testimonial.role || "";
 			if (testimonial.org) {
-				$_('.testimonial-role', tpl).appendChild(document.createElement("br"));
-				$_('.testimonial-role', tpl).appendChild(document.createTextNode(testimonial.org));
+				console.log($_('.testimonial-org', tpl), tpl);
+				// $_('.testimonial-role', tpl).prepend(document.createElement("br"));
+				$_('.testimonial-org', tpl).innerText = testimonial.org;
+				if (testimonial.org_link) {
+					$_('.testimonial-org', tpl).href = testimonial.org_link;
+				}
 			}
 			$_(`.testimonial-col:nth-child(${i%3 + 1})`).append(tpl);
 		}
