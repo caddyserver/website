8ebc51b5fa073e2a10906f98c61354c30eb94284
Matthew Holt <mholt@users.noreply.github.com>
2026-01-26 16:56:07 -0700

    ...and truncate(), sigh

--- src/old/resources/js/common.js
+++ src/old/resources/js/common.js
@@ -8,6 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 });
 
+function truncate(str, maxLen) {
+	if (!str) return "";
+	str = str.trim();
+	let firstPeriod = str.match(/\.(\s|$)/); // first dot not in the middle of a word, or at end of string
+	let terminate = firstPeriod ? firstPeriod.index+1 : str.length;
+	str = str.substring(0, terminate);
+	if (str.length <= maxLen) {
+		return str;
+	}
+	return str+"...";
+}
+
 function moduleDocsPreview(mod, maxLen) {
 	if (!mod || !mod.docs) return "";
 	let short = truncate(mod.docs, maxLen);
